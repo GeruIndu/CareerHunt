fa1cc1a5514e8771194dd34a76f96de7c1ddcf2b
GeruIndu <indrajitmondal284@gmail.com>
2025-03-28 13:45:21 +0530

    authentication middlewire  added

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { login, logout, register, updateProfile } from "../controllers/user.controller";
+import isAuthenticated from "../middlewires/isAuthenticate.js";
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/profile/update").post(updateProfile);
+router.route("/profile/update").post(isAuthenticated, updateProfile);
 router.route("/logout").get(logout);
 
 export default router;
