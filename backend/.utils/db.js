3433b107f5385c02066dcac3da57d655e1783949
GeruIndu <indrajitmondal284@gmail.com>
2025-04-14 12:38:35 +0530

    mongodb atlas ip resolved

--- backend/.utils/db.js
+++ backend/.utils/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("mongodb connect successfully");
     } catch (err) {
         console.log(err);
