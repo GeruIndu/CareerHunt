ad7a87ad7a626540b831413a3049311eb648ef0d
GeruIndu <indrajitmondal284@gmail.com>
2025-04-14 11:40:24 +0530

    routes are ready

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOption = {
-    origin: "http://localhost:5173",
+    origin: "https://careerhunt-6vwp.onrender.com",
     credentials: true
 }
 app.use(cors(corsOption))
